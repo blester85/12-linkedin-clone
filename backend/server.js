ba155cc5ab2ffa03ae0ea90c7bdb82c6d3cb7457
Brandon Lester <wblester85@gmail.com>
2026-01-19 20:55:56 -0500

    Finished lesson 200

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
